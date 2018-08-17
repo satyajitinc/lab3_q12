48187503851e73732afd2ecd35bb066746ac3e1f
satyajitinc <42139094+satyajitinc@users.noreply.github.com>
2018-08-17 16:21:11 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,23 @@
+// include library
+# include <iostream>
+using namespace std;
+//main fuction
+int main() {
+int sizeint; // declaration
+sizeint = sizeof (int);// intialization
+cout <<"The size of integer is "<< sizeint << endl;//prints on terminal
+int sizefloat;
+sizefloat = sizeof (float);
+cout <<"The size of float is "<< sizefloat<<"bytes"<< endl;
+int sizedouble;
+sizedouble = sizeof (double);
+cout<<"The size of double is "<< sizedouble << "bytes" << endl;
+int sizechar;
+sizechar = sizeof (char);
+cout <<"The size of character is "<< sizedouble << "bytes" << endl;
+int sizeboolean;
+sizeboolean = sizeof (bool);
+cout <<"The size of boolean is " << sizeboolean <<"bytes"<< endl;
+return 0;
+}
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,25 @@
+//include library
+#include <iostream>
+using namespace std;
+//main function
+int main()
+{
+int a,b,sum,substract,product,remainder ; //declaration
+float division;
+a = 5; //definition
+b = 3; //definition
+
+sum =a + b; //adds two numbers
+substract = a - b; // calculate the difference
+product = a * b; // calculate the product 
+division = a / b;
+//finds the remainder
+//displaying the calculations in the terminal
+cout <<"numbers are "<< a << "'" << b << endl;
+cout <<"sum is" << sum << endl;
+cout <<"difference is " << substract << endl;
+cout <<"product is " << product <<endl;
+cout <<"remainder is " << remainder << endl;
+return 0;
+}
+
